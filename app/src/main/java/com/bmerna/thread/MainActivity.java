b131ff075780a86f3a61ade25fdaedb1893abf69
Brendan Merna <bmernax30@gmail.com>
2016-01-12 10:01:34 -0500

    Put in a thread.

--- app/src/main/java/com/bmerna/thread/MainActivity.java
+++ app/src/main/java/com/bmerna/thread/MainActivity.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.Message;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            TextView text = (TextView) findViewById(R.id.text);
+            text.setText("Nice job!");
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void clickButton(View view) {
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                long futureTime = System.currentTimeMillis() + 10000;
+                while (System.currentTimeMillis() < futureTime) {
+                    synchronized (this) {
+                        try {
+                            wait(futureTime - System.currentTimeMillis());
+                        } catch (Exception e) {}
+                    }
+                }
+                handler.sendEmptyMessage(0);
+            }
+        };
+            Thread myThread = new Thread(r);
+            myThread.start();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
